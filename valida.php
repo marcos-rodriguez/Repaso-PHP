b04adf811523463b884c655f21fd3d07d4d915fb
Alejandro López López <redes_sociales@alejandrolopezlopez.com>
2018-02-22 17:02:32 -0600

    Destruir Sesión
    session destroy

--- valida.php
+++ valida.php
@@ -1,11 +1,19 @@
 <?php
 session_start();
+
+if(isset($_SESSION['login'])){
+ echo "<br>Bienvenido... ".$_SESSION['nombreusuario']."<a href='salir.php'>salir</a>";  
+}else{
+   
+
+
 $nombre=$_POST['form_usuario'];
 $nombre;
 
 if(isset($_POST['form_boton']) and (!empty($_POST['form_usuario'])) and (!empty($_POST['form_contrasena']))){
    echo "Bienvenido... ".$nombre;
    $_SESSION['nombreusuario']=$nombre;
+   $_SESSION['login']="yes";
    
    for($i=1;$i<10;$i++){
       echo "<br>Leer mensaje <a href='mostrarmensaje.php?id=".$i."'>".$i."</a>";
@@ -19,7 +27,7 @@ echo "<br>Hoy es ... ".$_SESSION['acceso']."<br>";
 }else{
     echo "debes llenar el <a href='index.php'>formulario</a>";
 }
-
+}
 
 
 ?>
